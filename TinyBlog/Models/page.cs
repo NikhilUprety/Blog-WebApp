b7a028045acabb054cc69430f3b7e1c12054459d
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-19 20:09:22 +0545

    model updated

---
+++ TinyBlog/Models/page.cs
@@ -0,0 +1,12 @@
+﻿namespace TinyBlog.Models
+{
+    public class page
+    {
+        public int Id { get; set; }
+        public string? Tittle { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Slug { get; set; }
+
+
+    }
+}
---
+++ TinyBlog/Models/post.cs
@@ -0,0 +1,19 @@
+﻿namespace TinyBlog.Models
+{
+    public class post
+
+    {
+    public int Id { get; set; }
+        public string? Title { get; set; }
+            public string? ShortDescription { get; set; }
+             public string? ApplicationUserID { get; set; }  
+        public ApplicationUser? ApplicationUser {  get; set; }  
+
+        public DateTime? CreatedTime { get; set; }
+        public string? Description { get; set; }    
+        public string? slug { get; set; }
+
+
+
+    }
+}
