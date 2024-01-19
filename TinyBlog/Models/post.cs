da3cdbfb8217ca4eb51d504a73623b0d399ff200
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-19 20:19:03 +0545

    post updated

--- TinyBlog/Models/post.cs
+++ TinyBlog/Models/post.cs
@@ -3,12 +3,11 @@
     public class post
 
     {
-    public int Id { get; set; }
+        public int Id { get; set; }
         public string? Title { get; set; }
-            public string? ShortDescription { get; set; }
-             public string? ApplicationUserID { get; set; }  
+        public string? ShortDescription { get; set; }
+        public string? ApplicationUserID { get; set; }  
         public ApplicationUser? ApplicationUser {  get; set; }  
-
         public DateTime? CreatedTime { get; set; }
         public string? Description { get; set; }    
         public string? slug { get; set; }
