8ce71e5a89616e8332d751a46a20d5d7ade99f22
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-24 15:30:23 +0545

    page controller added

---
+++ TinyBlog/Controllers/PageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinyBlog.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
