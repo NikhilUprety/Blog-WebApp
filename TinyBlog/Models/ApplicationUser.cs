397f72059e7e6d4ef49905c2cfa14cd7acd1fd02
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-25 13:04:19 +0545

    action aadded

--- TinyBlog/Models/ApplicationUser.cs
+++ TinyBlog/Models/ApplicationUser.cs
@@ -5,9 +5,8 @@ namespace TinyBlog.Models
     public class ApplicationUser : IdentityUser
     {
         public string? FirstName { get; set; }  
-            public string? LastName { get; set;}
+        public string? LastName { get; set;}
         public List<post>? posts { get; set; }
-
 
     }
 
