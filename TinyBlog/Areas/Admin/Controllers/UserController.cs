a30631ad89febe263c3859e96f0b4b2bbaedfc09
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-04 22:43:28 +0545

    db contsx

--- TinyBlog/Areas/Admin/Controllers/UserController.cs
+++ TinyBlog/Areas/Admin/Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TinyBlog.Areas.Admin.Controllers
 {
+    [Authorize]
     [Area("Admin")]
     public class UserController : Controller
     {
