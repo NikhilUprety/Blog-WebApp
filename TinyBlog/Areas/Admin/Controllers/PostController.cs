d82cd8d28d87efd2510c98f3b16440e4fe62244c
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-13 20:09:05 +0545

    post controller added

---
+++ TinyBlog/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinyBlog.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
