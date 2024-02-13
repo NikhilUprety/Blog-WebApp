fc433b2bf28ad623876ee94e3512164ad3f485c5
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-13 20:08:46 +0545

    program.cs

--- TinyBlog/Program.cs
+++ TinyBlog/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDi
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/login";
-    options.AccessDeniedPath = "/AccessDenied";
+   options.AccessDeniedPath = "/AccessDenied";
 });
 
 var app = builder.Build();
