0124c7489296fd4e7d422d05b2b64d62c000a69a
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-14 19:41:46 +0545

    post controller

--- TinyBlog/Areas/Admin/Controllers/PostController.cs
+++ TinyBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TinyBlog.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TinyBlog.Areas.Admin.Controllers
 {
     public class PostController : Controller
     {
         [Area("Admin")]
+        [Authorize] 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreatePostVMcs());
         }
     }
 }
