3ea762d4f435c8c0f8077f0407ad97f445ab5489
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-14 19:42:34 +0545

    migration update thumbnail

--- TinyBlog/Migrations/20240214092533_thumbnailurl.Designer.cs
+++ TinyBlog/Migrations/20240214092533_thumbnailurl.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TinyBlog.Data;
 
@@ -11,9 +12,10 @@ using TinyBlog.Data;
 namespace TinyBlog.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240214092533_thumbnailurl")]
+    partial class thumbnailurl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -259,7 +261,7 @@ namespace TinyBlog.Migrations
                     b.Property<string>("ApplicationUserID")
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<DateTime>("CreatedTime")
+                    b.Property<DateTime?>("CreatedTime")
                         .HasColumnType("datetime2");
 
                     b.Property<string>("Description")
