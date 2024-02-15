17183b7018212dea9e878a427a6836727f9e0751
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-15 22:10:43 +0545

    createpost vm added

--- TinyBlog/ViewModel/CreatePostVMcs.cs
+++ TinyBlog/ViewModel/CreatePostVMcs.cs
@@ -1,10 +1,13 @@
-﻿using TinyBlog.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using TinyBlog.Models;
 
 namespace TinyBlog.ViewModel
 {
     public class CreatePostVMcs
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? ApplicationUserID { get; set; }
