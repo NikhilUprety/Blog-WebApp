17fdd3d0097b448c185aabe26107a5ed2025ffc6
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-20 19:29:53 +0545

    page model updated

--- TinyBlog/Models/page.cs
+++ TinyBlog/Models/page.cs
@@ -5,7 +5,10 @@
         public int Id { get; set; }
         public string? Tittle { get; set; }
         public string? ShortDescription { get; set; }
+
+        public string? Description { get; set; }
         public string? Slug { get; set; }
+        public string? ThumbnailUrl { get; set; }
 
 
     }
