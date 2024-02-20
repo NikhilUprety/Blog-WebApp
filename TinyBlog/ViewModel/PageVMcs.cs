a943ac859d255f18da1960e7d1a123146b030eff
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-20 19:28:52 +0545

    page vm

---
+++ TinyBlog/ViewModel/PageVMcs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinyBlog.ViewModel
+{
+    public class PageVMcs
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Tittle { get; set; }
+        public string? ShortDescription { get; set; }
+
+        public string? Description { get; set; }
+        public IFormFile? Thumbnail { get; set; }
+        public string? ThumbnailUrl { get; set; }
+    }
+}
