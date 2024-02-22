a44a96b5e84a214db4a10b73322eaba60e914e8a
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-22 19:00:44 +0545

    pagecontroller action views added

--- TinyBlog/Controllers/PageController.cs
+++ TinyBlog/Controllers/PageController.cs
@@ -1,21 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using TinyBlog.Data;
+using TinyBlog.ViewModel;
 
 namespace TinyBlog.Controllers
 {
     public class PageController : Controller
     {
-        public IActionResult About()
+        private readonly ApplicationDbContext _context;
+
+        public PageController(ApplicationDbContext context)
         {
-            return View();
-        }
-        public IActionResult Contact()
-        {
-            return View();
+            _context = context;
         }
 
-        public IActionResult PrivacyPolicy()
+        public async Task<IActionResult> About()
         {
-            return View();
+            var about =await _context.PageTable!.FirstOrDefaultAsync(x => x.Slug == "about");
+            var vm = new PageVMcs()
+            {
+                Tittle = about!.Tittle,
+                ShortDescription= about!.ShortDescription,
+                Description= about!.Description,
+                ThumbnailUrl=about!.ThumbnailUrl,
+                
+            };
+            return View(vm);
+        }
+        public async Task<IActionResult> Contact()
+        {
+            var contact= await _context.PageTable!.FirstOrDefaultAsync(x => x.Slug == "contact");
+            var vm = new PageVMcs()
+            {
+                Tittle = contact!.Tittle,
+                ShortDescription = contact.ShortDescription,
+                Description = contact.Description,
+                ThumbnailUrl = contact.ThumbnailUrl,
+
+            };
+            return View(vm);
+            
+        }
+
+        public async Task<IActionResult> PrivacyPolicy()
+        {
+            var privacy = await _context.PageTable!.FirstOrDefaultAsync(x => x.Slug == "privacy");
+            var vm = new PageVMcs()
+            {
+                Tittle = privacy!.Tittle,
+                ShortDescription = privacy.ShortDescription,
+                Description = privacy.Description,
+                ThumbnailUrl = privacy.ThumbnailUrl,
+
+            };
+            return View(vm);
+
         }
     }
 }
