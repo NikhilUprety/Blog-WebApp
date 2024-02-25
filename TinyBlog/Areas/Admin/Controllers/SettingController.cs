ef7df56dde575dc0c6bef33447cbbace1beef288
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-25 21:14:35 +0545

    setting controller

---
+++ TinyBlog/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinyBlog.Areas.Admin.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
