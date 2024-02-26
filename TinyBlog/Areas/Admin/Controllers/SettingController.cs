3bde1edd5541d591034cb1238475d315cd12f8b8
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-26 23:29:45 +0545

    settingController

--- TinyBlog/Areas/Admin/Controllers/SettingController.cs
+++ TinyBlog/Areas/Admin/Controllers/SettingController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TinyBlog.Data;
+using TinyBlog.Models;
+using TinyBlog.ViewModel;
 
 namespace TinyBlog.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles ="Admin")]
     public class SettingController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public SettingController(ApplicationDbContext context, INotyfService notification, IWebHostEnvironment webHostEnvironment)
         {
-            return View();
+            _context = context;
+            _notification = notification;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            var setting = _context.settingTable!.ToList();
+            if(setting.Count > 0)
+            {
+                var settingvm = new SettingVM()
+                {
+                    Id = setting[0].Id,
+                    SiteName = setting[0].SiteName,
+                    ShortDescription = setting[0].ShortDescription,
+                    ThumbnailUrl = setting[0].ThumbnailUrl
+                };
+            }
+           return View();
         }
     }
 }
