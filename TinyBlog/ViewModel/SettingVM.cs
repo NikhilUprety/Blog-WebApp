47b8f8beb4d45b698f4f307660112ce1b6f5fe42
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-26 23:30:53 +0545

    vm

---
+++ TinyBlog/ViewModel/SettingVM.cs
@@ -0,0 +1,22 @@
+﻿namespace TinyBlog.ViewModel
+{
+    public class SettingVM
+
+    {
+
+        public string? Id { get; set; }
+        public string? SiteName { get; set; }
+        public string? ShortDescription { get; set; }
+
+        public string? ThumbnailUrl { get; set; }
+
+        public string? TwitterUrl { get; set; }
+
+        public string? LinkedinUrl { get; set; }
+
+        public string? GithubUrl { get; set; }
+
+        public IFormFile? Thumbnail {  get; set; }
+
+    }
+}
