15a6aa43c670bdb5bfdb4b1813f91489423f3c23
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-26 23:30:44 +0545

    setting model

---
+++ TinyBlog/Models/Setting.cs
@@ -0,0 +1,19 @@
+﻿namespace TinyBlog.Models
+{
+    public class Setting
+    {
+        public string? Id { get; set; }
+        public string?  SiteName { get; set; }
+        public string? ShortDescription { get; set; }
+
+        public string? ThumbnailUrl { get; set; }
+
+        public string? TwitterUrl {  get; set; }
+
+        public string? LinkedinUrl {get; set; }
+
+        public string? GithubUrl { get; set; }
+
+
+    }
+}
