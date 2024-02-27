17857a2880ed038b5864a645e3950392bfba481a
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-28 00:06:05 +0545

    home vm

---
+++ TinyBlog/ViewModel/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using TinyBlog.Models;
+
+namespace TinyBlog.ViewModel
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<post>? Posts { get; set; }
+    }
+}
