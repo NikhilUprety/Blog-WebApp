7b3b2c2f6a10cff07d47cdc529ae92c9ea06b183
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-28 00:00:15 +0545

    Home controller added

--- TinyBlog/Controllers/HomeController.cs
+++ TinyBlog/Controllers/HomeController.cs
@@ -1,21 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TinyBlog.Data;
 using TinyBlog.Models;
+using TinyBlog.ViewModel;
 
 namespace TinyBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context,
+                                INotyfService notyfService,
+                                IWebHostEnvironment webHostEnvironment,
+                                UserManager<ApplicationUser> userManager,
+                                ILogger<HomeController> logger)
         {
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
             _logger = logger;
         }
+
+        
             
         public IActionResult Index()
         {
-            return View();
+            var setting = _context.settingTable!.ToList();
+            var vm = new HomeVM()
+            {
+            Title = setting[0].Title,
+            ShortDescription = setting[0].ShortDescription,
+            ThumbnailUrl = setting[0].ThumbnailUrl,
+            Posts = _context.PostTable!.Include(x=>x.ApplicationUser).ToList(),
+
+        };
+            return View(vm);
         }
 
         public IActionResult Privacy()
