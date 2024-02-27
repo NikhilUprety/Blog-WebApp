935a78763f282c1546013530a08c7ae6a37b24ac
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-27 21:22:44 +0545

    setting controller

--- TinyBlog/Areas/Admin/Controllers/SettingController.cs
+++ TinyBlog/Areas/Admin/Controllers/SettingController.cs
@@ -1,6 +1,8 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TinyBlog.Data;
 using TinyBlog.Models;
 using TinyBlog.ViewModel;
@@ -8,34 +10,101 @@ using TinyBlog.ViewModel;
 namespace TinyBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class SettingController : Controller
     {
         private readonly ApplicationDbContext _context;
         public INotyfService _notification { get; }
         private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public SettingController(ApplicationDbContext context, INotyfService notification, IWebHostEnvironment webHostEnvironment)
+        public SettingController(ApplicationDbContext context,
+                                INotyfService notyfService,
+                                IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
-            _notification = notification;
+            _notification = notyfService;
             _webHostEnvironment = webHostEnvironment;
         }
 
-        public async  Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            var setting = _context.settingTable!.ToList();
-            if(setting.Count > 0)
+            var settings = _context.settingTable!.ToList();
+            if (settings.Count > 0)
             {
-                var settingvm = new SettingVM()
+                var vm = new SettingVM()
                 {
-                    Id = setting[0].Id,
-                    SiteName = setting[0].SiteName,
-                    ShortDescription = setting[0].ShortDescription,
-                    ThumbnailUrl = setting[0].ThumbnailUrl
+                    Id = settings[0].Id,
+                    SiteName = settings[0].SiteName,
+                    Title = settings[0].Title,
+                    ShortDescription = settings[0].ShortDescription,
+                    ThumbnailUrl = settings[0].ThumbnailUrl,
+                    LinkedinUrl= settings[0].LinkedinUrl,
+                    GithubUrl = settings[0].GithubUrl,
+                    TwitterUrl = settings[0].TwitterUrl,
                 };
+                return View(vm);
             }
-           return View();
+            var setting = new Setting()
+            {
+                SiteName = "Demo Name",
+            };
+            await _context.settingTable!.AddAsync(setting);
+            await _context.SaveChangesAsync();
+            var createdSettings = _context.settingTable!.ToList();
+            var createdVm = new SettingVM()
+            {
+                Id = createdSettings[0].Id,
+                SiteName = createdSettings[0].SiteName,
+                Title = createdSettings[0].Title,
+                ShortDescription = createdSettings[0].ShortDescription,
+                ThumbnailUrl = createdSettings[0].ThumbnailUrl,
+                LinkedinUrl = createdSettings[0].LinkedinUrl,
+                GithubUrl = createdSettings[0].GithubUrl,
+                TwitterUrl = createdSettings[0].TwitterUrl,
+            };
+            return View(createdVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SettingVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+            var setting = await _context.settingTable!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (setting == null)
+            {
+                _notification.Error("Something went wrong");
+                return View(vm);
+            }
+            setting.SiteName = vm.SiteName;
+            setting.Title = vm.Title;
+            setting.ShortDescription = vm.ShortDescription;
+            setting.LinkedinUrl = vm.LinkedinUrl;
+            setting.GithubUrl = vm.GithubUrl;
+            setting.TwitterUrl = vm.TwitterUrl;
+
+            if (vm.Thumbnail != null)
+            {
+                setting.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+            await _context.SaveChangesAsync();
+            _notification.Success("Setting updated succesfully");
+            return RedirectToAction("Index", "Setting", new { area = "Admin" });
+        }
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
+
+
