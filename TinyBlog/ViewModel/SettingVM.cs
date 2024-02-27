a71985e43d17bfb82ed427a8a82a5c9a827ca07c
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-28 00:01:24 +0545

    setting  viewmodel added

--- TinyBlog/ViewModel/SettingVM.cs
+++ TinyBlog/ViewModel/SettingVM.cs
@@ -1,11 +1,14 @@
-﻿namespace TinyBlog.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinyBlog.ViewModel
 {
     public class SettingVM
 
     {
-
-        public string? Id { get; set; }
+        [Key]   
+        public int Id { get; set; }
         public string? SiteName { get; set; }
+        public string? Title { get; set; }
         public string? ShortDescription { get; set; }
 
         public string? ThumbnailUrl { get; set; }
