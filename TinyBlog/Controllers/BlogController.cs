e210b6b96598d11ed97237af22c969bb61cf7272
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-02-28 00:00:00 +0545

    blog controller added

---
+++ TinyBlog/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using TinyBlog.Data;
+
+namespace TinyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+
+        public BlogController(ApplicationDbContext context, INotyfService notification)
+        {
+            _context = context;
+            _notification = notification;
+        }
+
+        public IActionResult post(string slug)
+        {
+            return View();
+        }
+    }
+}
